8af64361b82ad6aa5adf0cc4d7fc7d145a37a433
aaronfraint <aaronfraint@icloud.com>
2022-03-31 09:54:59 -0400

    turn layers on/off using dropdown

--- src/js/click.js
+++ src/js/click.js
@@ -2,11 +2,35 @@ let select_variable = document.getElementById("variable");
 
 const update_map_style = (map) => {
   select_variable.addEventListener("change", () => {
-    map.setPaintProperty("taz-total", "circle-radius", [
-      "/",
-      ["get", select_variable.value],
-      100,
-    ]);
+    if (select_variable.value == "total population") {
+      map.setLayoutProperty("taz-total-pop", "visibility", "visible");
+      map.setLayoutProperty("taz-total-emp-positive", "visibility", "none");
+      map.setLayoutProperty("taz-total-emp-negative", "visibility", "none");
+      map.setLayoutProperty("taz-pct-pop", "visibility", "none");
+      map.setLayoutProperty("taz-pct-emp-positive", "visibility", "none");
+      map.setLayoutProperty("taz-pct-emp-negative", "visibility", "none");
+    } else if (select_variable.value == "total employment") {
+      map.setLayoutProperty("taz-total-pop", "visibility", "none");
+      map.setLayoutProperty("taz-total-emp-positive", "visibility", "visible");
+      map.setLayoutProperty("taz-total-emp-negative", "visibility", "visible");
+      map.setLayoutProperty("taz-pct-pop", "visibility", "none");
+      map.setLayoutProperty("taz-pct-emp-positive", "visibility", "none");
+      map.setLayoutProperty("taz-pct-emp-negative", "visibility", "none");
+    } else if (select_variable.value == "pct population") {
+      map.setLayoutProperty("taz-total-pop", "visibility", "none");
+      map.setLayoutProperty("taz-total-emp-positive", "visibility", "none");
+      map.setLayoutProperty("taz-total-emp-negative", "visibility", "none");
+      map.setLayoutProperty("taz-pct-pop", "visibility", "visible");
+      map.setLayoutProperty("taz-pct-emp-positive", "visibility", "none");
+      map.setLayoutProperty("taz-pct-emp-negative", "visibility", "none");
+    } else if (select_variable.value == "pct employment") {
+      map.setLayoutProperty("taz-total-pop", "visibility", "none");
+      map.setLayoutProperty("taz-total-emp-positive", "visibility", "none");
+      map.setLayoutProperty("taz-total-emp-negative", "visibility", "none");
+      map.setLayoutProperty("taz-pct-pop", "visibility", "none");
+      map.setLayoutProperty("taz-pct-emp-positive", "visibility", "visible");
+      map.setLayoutProperty("taz-pct-emp-negative", "visibility", "visible");
+    }
   });
 };
 
